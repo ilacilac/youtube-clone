a4aed77029603f3df1674fcfe13be92c247c8a73
ilacilac <qkralswl307@gmail.com>
2021-01-04 23:19:46 +0900

    feat: Getting youtube videos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import VideoService from "./services/VideoService";
 
 function App() {
+  const youtube = new VideoService();
+
   return (
     <>
       <Header />
---
+++ src/services/VideoService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { apiKey } from "../key";
+
+class VideoService {
+  async getVideos() {
+    const response = await axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&key=${apiKey}`
+      )
+      .then((res) => res.data.items);
+
+    // const result = response.data.items;
+
+    // 데이터 가공
+    // const prettierResult = result.map((data) => {
+    //   // const result = {
+    //   //   title: part["title"],
+    //   //   channelTitle: part["channelTitle"],
+    //   //   thumbnails: part["thumbnails"],
+    //   // };
+    //   return data["snippet"]["title"];
+    // });
+    // console.log(response);
+    return response;
+  }
+}
+
+export default VideoService;
